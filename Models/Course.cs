91b54a4b2c22776dc93f5dd81384b16315a89a87
surejjaa <96488621+surejjaa@users.noreply.github.com>
2023-01-08 02:24:57 +0100

    1st

---
+++ Models/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1.Models
+{
+    public class Course
+    {
+        public string name { get; set; }
+
+        public string description { get; set; }
+
+        public string category { get; set; }
+
+        public string icon { get; set; }
+    }
+}
---
+++ ProfilePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test1.Models;
+
+namespace Test1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProfilePage : ContentPage
+    {
+        public List<Course> courses { get; set; }
+
+        public ProfilePage()
+        {
+            courses = new List<Course>
+            {
+                new Course {name="Web Development", category="IT", description="", icon=""},
+                new Course {name="Web Development", category="IT", description="", icon=""}
+            };
+
+            InitializeComponent();
+        }
+    }
+}
